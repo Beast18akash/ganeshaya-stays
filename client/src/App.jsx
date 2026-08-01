24e3348fff1290b4cd1cd19674866486bc3bde20
Akash Holsambre <holsambreakash18@gmail.com>
2026-08-01 19:26:51 +0530

    feat(client):added the responsive  navbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import Navbar from './components/Navbar'
+import { useLocation } from 'react-router-dom'
 
 const App = () => {
+
+  const iSOwnerPath = useLocation().pathname.includes("owner");
   return (
-    <div>App</div>
+    <>  
+    {/* 
+    if you are the owner then i will not show you the navigation bar
+    */}
+    {!iSOwnerPath &&  <Navbar/>}
+    </>
   )
 }
 
