e5ae5a3ce36a0b3f8f232a7e983a281dbc86fd19
Akash Holsambre <holsambreakash18@gmail.com>
2026-08-03 20:02:29 +0530

    feat(client): add home page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,12 +2,13 @@ import React from 'react'
 import Navbar from './components/Navbar'
 import { Route, Routes, useLocation } from 'react-router-dom'
 import Home from './pages/Home';
+import Footer from './components/Footer';
 
 const App = () => {
 
   const iSOwnerPath = useLocation().pathname.includes("owner");
   return (
-    <>  
+    <div>
     {/* 
     if you are the owner then i will not show you the navigation bar
     */}
@@ -17,7 +18,8 @@ const App = () => {
         <Route path ='/' element={<Home/>}/>
       </Routes>
     </div>
-    </>
+    <Footer/>
+    </div>
   )
 }
 
