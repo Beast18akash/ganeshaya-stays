81100d94202426c3f9b577e1400af68369f24bae
Akash Holsambre <holsambreakash18@gmail.com>
2026-08-05 22:11:25 +0530

    feat(client): add room details page

--- client/src/pages/AllRooms.jsx
+++ client/src/pages/AllRooms.jsx
@@ -72,7 +72,7 @@ const sortOptions = [
                     {/* Room Amenities */}
                     <div className='flex flex-wrap items-center mt-3 mb-6 gap-4'>
                 {room.amenities.map((item,index)=>(
-                    <div ke={index} className='flex items-center gap-2 px-3 py-2 rounded-lg bg-[#F5F5FF]/70'> 
+                    <div key={index} className='flex items-center gap-2 px-3 py-2 rounded-lg bg-[#F5F5FF]/70'> 
                         <img  src ={facilityIcons[item]} alt ={item} className='w-5 h-5'/>
                         <p className='text-xs' >{item}</p>
                     </div>
